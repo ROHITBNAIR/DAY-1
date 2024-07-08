5fb84589e9933c7952fda9ccbb82a807e2a2e3f3
Rohit B Nair <rohitbnair1@gmail.com>
2024-07-08 16:52:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Welcome");
+let value=6;
+let a=value+5;
+{
+    let value=7;
+    console.log("the output value will be: "+value);
+}
+
+console.log("the output value will be: "+value);
+
